8d80fe6de7c07d129f010ca00dacacce298994ab
aniket-pharale <43331903+aniket-pharale@users.noreply.github.com>
2024-02-09 15:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
+'use strict';
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
 app.get('/', (req, res) => {
-    // console.log("started ")
-    // const { NodeTracerProvider } = require('@opentelemetry/node');
-    // const { SimpleSpanProcessor, ConsoleSpanExporter } = require('@opentelemetry/tracing');
-    // const { StackdriverTraceExporter } = require('@opentelemetry/exporter-stackdriver-trace');
+  res.send('Hello World Ramesh');
+});
 
-    // const provider = new NodeTracerProvider();
-
-    // // Add an exporter for Stackdriver Trace
-    // const exporter = new StackdriverTraceExporter({
-    //     projectId: 'prj-chng-apps-nft-nprod-1',
-    // });
-
-    // // Optionally, you can also export traces to the console for testing
-    // const consoleExporter = new ConsoleSpanExporter();
-    // console.log("console.")
-    // // Use BatchSpanProcessor to batch and send spans to the exporter
-    // const spanProcessor = new SimpleSpanProcessor(exporter);
-    // provider.addSpanProcessor(spanProcessor);
-    // provider.addSpanProcessor(new SimpleSpanProcessor(consoleExporter));
-
-    // provider.register();
-
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
